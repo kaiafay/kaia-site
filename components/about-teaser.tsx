de740f2c99d7cf6de9ad3d0fcfdbe9025e6fb11f
Kaia Scheirman <kfscheirman18@gmail.com>
2026-06-14 17:56:44 -0600

    clarify copy for about section

--- components/about-teaser.tsx
+++ components/about-teaser.tsx
@@ -21,7 +21,7 @@ export function AboutTeaser() {
             personal interest. Turns out I had a brain for it. I accelerated
             through a full-stack engineering program in 2022, graduated at the
             top of my cohort, and haven&apos;t stopped building since. Today
-            I&apos;m taking on freelance client work and shipping my own
+            I build websites and web apps for clients, alongside my own
             products.
           </p>
           <Link
