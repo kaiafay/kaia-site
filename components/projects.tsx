d3bea62981a6be34c127143078e35efbfc92fa45
Kaia Scheirman <kfscheirman18@gmail.com>
2026-06-15 14:35:44 -0600

    Fix three-card grid orphan layout at tablet breakpoint

--- components/projects.tsx
+++ components/projects.tsx
@@ -65,7 +65,7 @@ export function Projects({
           </SectionHeading>
         </div>
 
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 md:grid-cols-3">
           {displayProjects.map((project, i) => {
             const Icon = ICON_MAP[project.icon] ?? Loader2;
             return (
