d23fc0e12b20dea0b1a56c585535a92d58f6fe88
Kaia Scheirman <kfscheirman18@gmail.com>
2026-06-15 18:31:26 -0600

    jump to contact portion of page

--- components/freelance-services.tsx
+++ components/freelance-services.tsx
@@ -39,7 +39,7 @@ const services = [
 ];
 
 function contactHref(interest: string) {
-  return `/work-with-me?interest=${encodeURIComponent(interest)}`;
+  return `/work-with-me?interest=${encodeURIComponent(interest)}#contact`;
 }
 
 export function FreelanceServices() {
